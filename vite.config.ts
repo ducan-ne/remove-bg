9a4a4150f12426c5c301404c79fe4b879e00009b
An <16853175+ducan-ne@users.noreply.github.com>
2024-08-26 01:59:36 +0700

    chore: Update npm dependencies and add detect-gpu package

--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,29 @@ import cssInject from "vite-plugin-css-injected-by-js"
 import { resolve } from "node:path"
 
 export default defineConfig({
-  plugins: [
-    react(),
-    cssInject(),
-  ],
-  resolve: process.env.NODE_ENV === "production" ? {
-    alias: {
-      '@huggingface/transformers': 'https://cdn.jsdelivr.net/npm/@huggingface/transformers@3.0.0-alpha.9'
-    }
-  } : {},
+  plugins: [react(), cssInject()],
+  resolve:
+    process.env.NODE_ENV === "production"
+      ? {
+          alias: {
+            "@huggingface/transformers":
+              "https://cdn.jsdelivr.net/npm/@huggingface/transformers@3.0.0-alpha.9",
+          },
+        }
+      : {},
   build: {
     target: "esnext",
-    rollupOptions: {
-      external: ["react/jsx-runtime", "react", "react-dom"],
-      input: resolve(__dirname, "src/App.tsx"),
-      preserveEntrySignatures: "exports-only",
-      output: {
-        entryFileNames: "index.js",
-        format: "esm",
-      },
-    },
+    rollupOptions:
+      process.env.TARGET === "bannerify"
+        ? {
+            external: ["react/jsx-runtime", "react", "react-dom"],
+            input: resolve(__dirname, "src/App.tsx"),
+            preserveEntrySignatures: "exports-only",
+            output: {
+              entryFileNames: "index.js",
+              format: "esm",
+            },
+          }
+        : {},
   },
 })
